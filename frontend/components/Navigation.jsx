50689139636dbc5e1e85b059ec16b707fcd631ee
jjplaza-dev <jjplaza.dev@gmail.com>
2025-03-24 02:12:16 +0800

    Vite React setup, minor frontend

---
+++ frontend/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Navigation() {
+  return <>
+    <section className='w-screen h-20 bg-green-500 sticky top-0 my-5'>NAVIGATION</section>
+  
+  </>
+}
+
+export default Navigation
---
+++ frontend/pages/Front.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Front() {
+  return <>
+    <section className='w-screen h-screen'>Front</section>
+  </>
+}
+
+export default Front
